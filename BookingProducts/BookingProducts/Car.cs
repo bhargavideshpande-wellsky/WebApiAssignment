f33e4fd16577ebd3e95b40f9113d1730772cb5d8
Bhargavi Deshpande <bdeshpande@tavisca.com>
2018-09-05 14:08:36 +0530

    Factory Method Implement

---
+++ BookingProducts/BookingProducts/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingProducts
+{
+    class Car : Iproduct
+    {
+        public void Book()
+        {
+            Console.WriteLine("Car Booked");
+        }
+
+        public string GetTypeOfProduct()
+        {
+            return "car";
+        }
+
+        public void Save()
+        {
+            Console.WriteLine("Car saved");
+        }
+    }
+}
---
+++ BookingProducts/BookingProducts/IproductFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingProducts
+{
+    public class IproductFactory
+    {
+        public Iproduct GetProduct(string productName)
+        {
+            switch (productName)
+            {
+                case "car": return new Car();
+                case "activity": return new Activity();
+                case "hotel": return new Hotel();
+                case "air": return new Air();
+                default: return new Air();
+            }
+                
+        }
+    }
+}
