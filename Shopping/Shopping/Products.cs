12e086b2f857483dbfaec9214ee9a5bb62c278d1
Bhargavi Deshpande <bdeshpande@tavisca.com>
2018-09-06 10:24:39 +0530

    Made the changes as per requirement

---
+++ Shopping/Shopping/Products.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shopping
+{
+    class Products
+    {
+        public int ItemId { get; set; }
+        public string ItemName { get; set; }
+        public int ItemPrice { get; set; }
+    }
+}
