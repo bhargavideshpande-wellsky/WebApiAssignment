8328657f57ba5f23f0b5f0bc18368fa7bd1ced60
Bhargavi Deshpande <bdeshpande@tavisca.com>
2018-09-07 19:33:30 +0530

    assignment of 3 design patterns

---
+++ BookingProductsRevised/BookingProducts/BookingProducts/Logging.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingProducts
+{
+    public sealed class Logging
+    {
+        private static Logging instance=null;
+        private Logging()
+        {
+
+        }
+        public static Logging Instance
+        {
+           get
+           {
+             if (instance == null)
+             {
+                    instance = new Logging();
+             }
+                return instance;
+           }
+              
+        }
+        public void AddLoggingDetails(string msg)
+        {
+            string pathString = @"C:\LoggingDetails.txt";
+
+            using (StreamWriter sw = File.AppendText(pathString))
+            {
+                sw.WriteLine(msg);
+                
+            }
+        }
+    }
+}
---
+++ BookingProductsRevised/BookingProducts/BookingProducts/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingProducts
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            productFactory obj = new productFactory();
+            Repository obj1 = new Repository();
+            FareStrategy obj2 = new FareStrategy();
+            Console.WriteLine("Enter Product which you want to book or save [eg: car,air,activity,hotel]");
+            string productName = Console.ReadLine();
+            Iproduct prod = obj.GetProduct(productName);
+
+            string product = prod.GetTypeOfProduct();
+
+            Console.WriteLine("Enter the storage type [eg: file,database]");
+            string storageType = Console.ReadLine();
+            Irepository storage = obj1.GetStorageType(storageType);
+
+            IFareStrategy fareOfType = obj2.GetFare(productName);
+            
+            Console.WriteLine("Enter your choice\n1.save "+product+"\n2.Book "+product);
+            int ch = Convert.ToInt32(Console.ReadLine());
+            switch (ch)
+            {
+                case 1:
+                    prod.Save(prod, storage, fareOfType.CalculateFare(prod.Fare));
+                    break;
+                case 2:
+                    prod.Book(prod,storage, fareOfType.CalculateFare(prod.Fare));
+                    break;
+
+            }
+           
+        }
+    }
+}
