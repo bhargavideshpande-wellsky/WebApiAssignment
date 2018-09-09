2fc85656a9d626b6ad26bb3943b5591337a693be
Bhargavi Deshpande <bdeshpande@tavisca.com>
2018-09-09 21:12:24 +0530

    Api for booking product

---
+++ ProductBookingAPI/ProductBookingAPI/Controllers/CarController.cs
@@ -0,0 +1,51 @@
+﻿using ProductBookingAPI.Models;
+using ProductDataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ProductBookingAPI.Controllers
+{
+    public class CarController : ApiController
+    {
+        public IEnumerable<Car> GetCar()
+        {
+            using (BookProductAPIEntities entities = new BookProductAPIEntities())
+            {
+                return entities.Cars.ToList();
+            }
+        }
+        public void PostCar([FromBody]Car car)
+        {
+            using (BookProductAPIEntities entities = new BookProductAPIEntities())
+            {
+                int id = Int32.Parse(entities.Cars.Max(p => p.Id).ToString());
+                id++;
+                car.Id = id;
+                entities.Cars.Add(car);
+                entities.SaveChanges();
+            }
+        }
+        public void PutCar([FromBody] BookSave product)
+        {
+            using (BookProductAPIEntities entities = new BookProductAPIEntities())
+            {
+                var car = entities.Cars.Find(product.id);
+                if (product.action == "book")
+                {
+                    car.IsBooked = true;
+                    entities.SaveChanges();
+                }
+                else if (product.action == "save")
+                {
+                    car.IsSaved = true;
+                    entities.SaveChanges();
+                }
+
+            }
+        }
+    }
+}
---
+++ ProductBookingAPI/ProductBookingAPI/Controllers/HotelController.cs
@@ -0,0 +1,51 @@
+﻿using ProductBookingAPI.Models;
+using ProductDataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ProductBookingAPI.Controllers
+{
+    public class HotelController : ApiController
+    {
+        public IEnumerable<Hotel> GetHotel()
+        {
+            using (BookProductAPIEntities entities = new BookProductAPIEntities())
+            {
+                return entities.Hotels.ToList();
+            }
+        }
+        public void PostHotel([FromBody]Hotel hotel)
+        {
+            using (BookProductAPIEntities entities = new BookProductAPIEntities())
+            {
+                int id = Int32.Parse(entities.Hotels.Max(p => p.Id).ToString());
+                id++;
+                hotel.Id = id;
+                entities.Hotels.Add(hotel);
+                entities.SaveChanges();
+            }
+        }
+        public void PutHotel([FromBody] BookSave product)
+        {
+            using (BookProductAPIEntities entities = new BookProductAPIEntities())
+            {
+                var hotel = entities.Hotels.Find(product.id);
+                if (product.action == "book")
+                {
+                    hotel.IsBooked = true;
+                    entities.SaveChanges();
+                }
+                else if (product.action == "save")
+                {
+                    hotel.IsSaved = true;
+                    entities.SaveChanges();
+                }
+
+            }
+        }
+    }
+}
